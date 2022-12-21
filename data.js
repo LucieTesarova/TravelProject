899901751b718f6f4a6823664a7febc18eb7152b
Lucie Tesarova <lucka2121@gmail.com>
2022-12-21 12:20:16 +0100

    initialize commit

---
+++ data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Sydney",
+        location: "Australia",
+        googleMapsUrl: "https://www.google.com/maps/place/Sydney",
+        startDate: "19 June, 2018",
+        endDate: "28 June, 2018",
+        description: "Sydney is the best town in Australia, it is a very cultivate city with a lot of interesting places. You never be bored in Sydney.",
+        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    },
+    {
+        title: "Nusfojrd",
+        location: "Norway",
+        googleMapsUrl: "https://www.google.com/maps/place/8380+Nusfjord",
+        startDate: "08 Feb, 2015",
+        endDate: "17 Feb, 2015",
+        description: "Nusfojrd is a magic place. When you arrive there you feel like time is stopped. The nature is fenomenal, the people are so kind and the whole village seem very cute.",
+        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    }
+]
